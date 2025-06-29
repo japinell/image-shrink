11d763758de363a8dc35c3997b5c660b52f93458
Jose A. Pinell <japinell@yahoo.com>
2025-06-28 20:17:28 -0700

    Added event handlers

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, Menu } = require('electron');
+
+// Set environment variable for development
+process.env.NODE_ENV = 'development';
+const isDev = process.env.NODE_ENV === 'development';
+const isWin = process.platform === 'win32';
 
 const createMainWindow = () => {
     const mainWindow = new BrowserWindow(
@@ -6,11 +11,28 @@ const createMainWindow = () => {
             title: 'Image Shrink',
             width: 500,
             height: 600,
+            icon: './assets/icons/Icon_256x256.png',
+            resizable: isDev ? true: false,
         }
     )
+
+    //mainWindow.loadURL(`file://${__dirname}/app/index.html`);
+    mainWindow.loadFile('./app/index.html');
 
 }
 
 app.on('ready', () => {
     createMainWindow();
+})
+
+app.on('window-all-closed', () => {
+    if (isWin) {
+        app.quit();
+    }
+})
+
+app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createMainWindow();
+    }
 })
